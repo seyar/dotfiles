298ed05ddcd0e44a068effb6c5a65e5bf4d66a18
Seyar Chapuh <seyar@yandex-team.ru>
2013-02-09 00:00:20 +0200

    добавлена дефолт таймзона

--- sorter.php
+++ sorter.php
@@ -1,4 +1,4 @@
-<?
+<?php
 $arguments = getopt("a::",array("help::"));
 
 if(isset($arguments['help']) ){
@@ -8,9 +8,12 @@ if(isset($arguments['help']) ){
 	die;
 }
 
-define( 'PROD', (isset($arguments['a']) && $arguments['a'] === FALSE) );
-define( 'DOCROOT', '/home/seyar/photos');
-if( PROD ) error_reporting(0);
+date_default_timezone_set('Europe/Kiev');
+
+define( 'ALL', (isset($arguments['a']) && $arguments['a'] === FALSE) );
+define( 'DOCROOT', getcwd() );
+//if( ALL ) error_reporting(0);
+
 
 $files = scandir(DOCROOT);
 array_shift($files);
@@ -19,23 +22,23 @@ $i = 0;
 $folders = array();
 foreach( $files as $item)
 {
-	if($i > 5 && !PROD ) die("5 вышло\n"); 
+	if($i > 5 && !ALL ) die("5 вышло\n");
 
 	if(is_file($item) && (strtolower(substr($item, -3)) == 'jpg'))
 	{
 		$i++;
 		$image = DOCROOT.'/'.$item;
-		//if( !is_file($image) ) die('не файло'."\n");		
+		//if( !is_file($image) ) die('не файло'."\n");
 		$exif = exif_read_data($image, 0, true);
 
 		$date = (isset($exif['EXIF']['DateTimeOriginal']) ? $exif['EXIF']['DateTimeOriginal'] : (isset($exif['EXIF']['DateTimeDigitized']) ? $exif['EXIF']['DateTimeDigitized'] : '' ) );
 		$camera = isset($exif['IFD0']['Model']) ?  str_replace(array(' ', '/','\\',','),'_',$exif['IFD0']['Model']) : '';
 		$path = generatePath($date, $camera);
 		if( !$path ) continue;
-		
+
 		$fullpath = DOCROOT.'/'.$path;
-		if( !is_dir($fullpath) ) mkdir($fullpath,0755,true);			
-			
+		if( !is_dir($fullpath) ) mkdir($fullpath,0755,true);
+
 		if( copy($image, $fullpath.basename($image)) )
 		{
 			 echo basename($image)." - moved to ".dirname($fullpath)."\n";
@@ -43,7 +46,7 @@ foreach( $files as $item)
 		}
 		else
 			echo $image.' = '.$fullpath.basename($image)." not moved\n";
-						
+
 	}
 }
 die();
@@ -52,7 +55,7 @@ die();
 function generatePath( $date = NULL, $camera = '' )
 {
 	if( !isset($date) || empty($date) ) return false;
-	
+
 	$path = date('Y/n-F/', strtotime($date)).$camera.'/';
 	return $path;
 }
@@ -65,24 +68,24 @@ $i = 0;
 $folders = array();
 foreach( $files as $item)
 {
-	
-	if($i > 5 && (!isset($argv[1]) || $argv[1] != 'prod')) die(); 
+
+	if($i > 5 && (!isset($argv[1]) || $argv[1] != 'prod')) die();
 
 	if(is_file($item))
 	{
 		$i++;
 		$image = dirname(__FILE__).'/'.$item;
 		if( !is_file($image) ) die('не файло'."\n");
-		
+
 		$exif = exif_read_data($image, 0, true);
-		
+
 		if($exif):
 		foreach ($exif as $key => $section) {
-			foreach ($section as $name => $val) 
+			foreach ($section as $name => $val)
 			{
 				if( $key.$name == 'IFD0Model' )
 					$folders[] = str_replace(' ','_',$val);
-			}	
+			}
 		}
 		endif;
 	}
