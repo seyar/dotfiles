42b94a091b7794346fb4481fe4443e65ca31f4d9
Seyar Chapuh <seyar@yandex-team.ru>
2013-03-13 18:01:23 +0200

    Изменены настроечки на свои

--- .bash/aliases.git.bash
+++ .bash/aliases.git.bash
@@ -1,21 +1,35 @@
 # git aliases (based on TJ Holowaychuk's aliases)
-alias gd="git di"
 alias gdc="git dic"
-alias ga="git add"
 alias gbd="git branch -D"
-alias gst="git status"
-alias gc="git commit -m"
-alias gca="git commit -a -m"
 alias gm="git merge --no-ff"
 alias gpt="git push --tags"
 alias gp="git push"
-alias grh="git reset --hard"
-alias gb="git branch"
 alias gcob="git checkout -b"
 alias gco="git checkout"
-alias gba="git branch -a"
-alias gcp="git cherry-pick"
-alias glg="git lg"
-alias gh="git hist"
-alias gpom="git pull origin master"
 alias gr='cd "`git rev-parse --show-toplevel`"'
+
+alias gs='git status -s'
+alias ga='git add '
+alias gb='git branch '
+alias gc='git commit'
+alias go='git checkout '
+
+alias gsl='git status | less'
+alias gl='git log'
+
+alias ggom='git pull origin master'
+alias gsom='git push origin master'
+alias ggod='git pull origin dev'
+alias gsod='git push origin dev'
+
+alias gom='git checkout master'
+alias grh='git reset --hard'
+alias gcl='git clean -f -d'
+alias gcp='git cherry-pick'
+alias gfo='git fetch origin'
+alias grm='git rm -r'
+alias gms='git merge --squash'
+alias gabp='git add blocks/ pages/'
+alias grsh='git reset --soft HEAD^'
+alias gh='git hist'
+alias gd='git diff'
