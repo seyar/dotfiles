19e8b50d5cc0ed7e2f6738a68e1cd19ddb9d6125
Seyar Chapuh <seyar@yandex-team.ru>
2013-08-28 11:27:14 +0300

    Переписан сортировщик на шелл

--- sorter.sh
+++ sorter.sh
@@ -1,14 +1,52 @@
-#!/bin/sh 
-BASEDIR="/share/photos" 
-LIST=`find . -iname "*.jpg"` 
+#!/bin/bash 
+#Входная папка
+BASEDIR="."
+#Бекапный винт
+BACKUPDIR="/Volumes/Time Machine/"
+#список не вложенных jpg файлов
+#LIST=`find . -iname "*.jpg" -print0 -d 1`
+LIST=`ls | grep -i  ".jpg"`
+#строка для грепа даты из экзива 
 STRTIME="Image timestamp" 
- 
-for F in $LIST; do 
-YEAR=`exiv2 "$F" | grep "${STRTIME}" | awk '{ print $4 }' | awk -F: '{ print $1 }'` 
-MONTH=`exiv2 "$F" | grep "${STRTIME}" | awk '{ print $4 }' | awk -F: '{ print $2 }'` 
-DAY=`exiv2 "$F" | grep "${STRTIME}" | awk '{ print $4 }' | awk -F: '{ print $3 }'` 
-NAME=`basename "$F"` 
-DIR="${BASEDIR}/${YEAR}/${MONTH}/$DAY" 
-mkdir -p ${DIR} 
-mv -f ${F} "${DIR}/${NAME}" 
+#строка для грепа камеры из экзив инфы
+CAMERAMODEL="Camera model"
+
+#название папок месяцами
+MONTHS[1]='1-January'
+MONTHS[2]='2-February'
+MONTHS[3]='3-March'
+MONTHS[4]='4-April'
+MONTHS[5]='5-May'
+MONTHS[6]='6-June'
+MONTHS[7]='7-July'
+MONTHS[8]='8-August'
+MONTHS[9]='9-September'
+MONTHS[10]='10-Octovber'
+MONTHS[11]='11-November'
+MONTHS[12]='12-December'
+
+find . -iname "*.jpg" -d 1 -print0 | while read -d $'\0' F 
+do
+  #Год
+  YEAR=`exiv2 "$F" | grep "${STRTIME}" | awk '{ print $4 }' | awk -F: '{ print $1 }'` 
+  #Месяц ведущего без нуля
+  MONTH=`exiv2 "$F" | grep "${STRTIME}" | awk '{ print $4 }' | awk -F: '{ print $2 }' | sed -e s/0//` 
+  #день
+  #DAY=`exiv2 "$F" | grep "${STRTIME}" | awk '{ print $4 }' | awk -F: '{ print $3 }'` 
+  #модель камеры без инфы грепа и замена пробелов на слеш
+  MODEL=`exiv2 "$F" | grep "${CAMERAMODEL}" | sed -e "s/^.*:.//" | sed "s/ /_/g"`
+  NAME=`basename "$F"` 
+  #папка куда перемещать
+  DIR="${BASEDIR}/${YEAR}/${MONTHS[$MONTH]}/${MODEL}" 
+  mkdir -p "${DIR}" 
+  mv -f "${F}" "${DIR}/${NAME}"
+  echo "${DIR}/${NAME}" 
 done
+
+if [ -d "${BACKUPDIR}photos/" ]; then 
+  #забекапим на внешний винт. Все вместе с симлинкой
+  rsync -r -L $BASEDIR "${BACKUPDIR}photos/"
+  echo "Синхронизировано с ${BACKUPDIR}"
+else
+  echo "Не синхронизировано."
+fi;
