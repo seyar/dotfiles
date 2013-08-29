e24f4dbc61ce0cfc432d3fa350bd5b90311954df
Seyar Chapuh <seyar@yandex-team.ru>
2013-08-29 20:23:09 +0300

    Поправлен сортировщик фото. Добавлен видео сортировщик

--- video-sorter.sh
+++ video-sorter.sh
@@ -1,14 +1,15 @@
-#!/bin/bash 
+#!/bin/bash
 #@depends on exiv2
 #Входная папка
-BASEDIR="."
+BASEDIR="./video"
 #Бекапный винт
 BACKUPDIR="/Volumes/Time Machine/"
 #список не вложенных jpg файлов
 #LIST=`find . -iname "*.jpg" -print0 -d 1`
 #LIST=`ls | grep -i  ".jpg"`
-#строка для грепа даты из экзива 
-STRTIME="Image timestamp" 
+#строка для грепа даты из экзива
+STRTIME="^Create Date"
+STRTIME2="Date/Time Original"
 #строка для грепа камеры из экзив инфы
 CAMERAMODEL="Camera model"
 
@@ -26,19 +27,23 @@ MONTHS[10]='10-Octovber'
 MONTHS[11]='11-November'
 MONTHS[12]='12-December'
 
-find . -iname "*.jpg" -d 2 -print0 | while read -d $'\0' F 
+echo "Введите название события: "
+read EVENT 
+
+find -E  . -iregex ".*\.(mov|3gp|mp4|avi)" -d 1 -print0 | while read -d $'\0' F 
 do
   #Год
-  YEAR=`exiv2 "$F" | grep -a "${STRTIME}" | awk '{ print $4 }' | awk -F: '{ print $1 }'` 
+  YEAR=`exiftool "$F" | grep -a "${STRTIME}" | awk '{ print $4 }' | awk -F: '{ print $1 }'` 
   #Месяц ведущего без нуля
-  MONTH=`exiv2 "$F" | grep -a "${STRTIME}" | awk '{ print $4 }' | awk -F: '{ print $2 }' | sed -e s/0//` 
+  MONTH=`exiftool "$F" | grep -a "${STRTIME}" | awk '{ print $4 }' | awk -F: '{ print $2 }' | sed -e s/0//` 
   #день
-  #DAY=`exiv2 "$F" | grep "${STRTIME}" | awk '{ print $4 }' | awk -F: '{ print $3 }'` 
+  #DAY=`exiftool "$F" | grep "${STRTIME}" | awk '{ print $4 }' | awk -F: '{ print $3 }'` 
   #модель камеры без инфы грепа и замена пробелов на слеш
-  MODEL=`exiv2 "$F" | grep -a "${CAMERAMODEL}" | sed -e "s/^.*:.//" | sed "s/ /_/g"`
+  #MODEL=`exiv2 "$F" | grep -a "${CAMERAMODEL}" | sed -e "s/^.*:.//" | sed "s/ /_/g"`
   NAME=`basename "$F"` 
   #папка куда перемещать
-  DIR="${BASEDIR}/${YEAR}/${MONTHS[$MONTH]}/${MODEL}" 
+  DIR="${BASEDIR}/${YEAR}/${MONTHS[$MONTH]}/${EVENT// /_}"
+
   mkdir -p "${DIR}" 
   mv -f "${F}" "${DIR}/${NAME}"
   echo "${DIR}/${NAME}" 
