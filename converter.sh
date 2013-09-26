605b5acb4a692ce3fc61805263660231d66f3192
Seyar Chapuh <seyarchapuh@ya.ru>
2013-09-26 14:45:24 +0300

    ДОбавил различие по имени в конвертируемых видео файлах. чтоб не перетереть

--- converter.sh
+++ converter.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#Видео сжиматор. 
+#Видео сжиматор.
 #depends HandBrakeCli
 
 for f in *.MOV
 do
-    HandBrakeCLI -i $f -o ${f:0:-3}mp4 -e x264 -q 20 -B 160
+    HandBrakeCLI -i $f -o ${f:0:-2}_converted.mp4 -e x264 -q 20 -B 160 --keep-display-aspect
 done
 rm *.MOV
