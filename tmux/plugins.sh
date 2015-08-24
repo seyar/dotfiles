aba98a3b96df70e7c1d21df4e3f8a25c91705f08
Seyar Chapuh <seyar@yandex-team.ru>
2015-08-25 01:32:45 +0300

    tmux improvements

---
+++ tmux/plugins.sh
@@ -0,0 +1,9 @@
+# List of plugins
+set -g @plugin 'tmux-plugins/tpm'
+set -g @plugin 'tmux-plugins/tmux-sensible'
+
+# сохранение сессии в файл
+set -g @plugin 'tmux-plugins/tmux-resurrect'
+
+# Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
+run '$HOME/.configs/tpm/tpm'
