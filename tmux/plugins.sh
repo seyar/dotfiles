8dfd87e4973521aaa7c3927a98556117c1875926
Seyar Chapuh <seyar@yandex-team.ru>
2015-09-29 16:38:15 +0300

    dotfiles folder to string

--- tmux/plugins.sh
+++ tmux/plugins.sh
@@ -6,4 +6,4 @@ set -g @plugin 'tmux-plugins/tmux-sensible'
 set -g @plugin 'tmux-plugins/tmux-resurrect'
 
 # Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
-run '$HOME/$DOTFILES_FOLDER/tpm/tpm'
+run '$HOME/.dotfiles/tpm/tpm'
