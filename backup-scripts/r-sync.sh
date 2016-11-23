88f372c52fa52f2ce2bb590fa0f2efc914ed91ed
Seyar Chapuh <seyar@yandex-team.ru>
2016-11-23 16:18:23 +0300

    dont remove backing up files

--- backup-scripts/r-sync.sh
+++ backup-scripts/r-sync.sh
@@ -2,16 +2,16 @@
 #что синкаем
 BASEDIR="/Users/seyar/media"
 #Бекапный винт
-BACKUPDIR="/Volumes/FAT/"
+BACKUPDIR="/Volumes/Data/"
 #в какой папке мы запустили скрипт, такую же создадим на бекапном и положим файлы
 FOLDER=${BACKUPDIR}`basename "${BASEDIR}"`
 
 if [ -d "${BACKUPDIR}" ]; then
   mkdir -p   "${FOLDER}"
   #забекапим на внешний винт. Все вместе с симлинкой
-  rsync -r -L "$BASEDIR/" "${FOLDER}"
-  rm -rf "$BASEDIR/photos"
-  rm -rf "$BASEDIR/video"
+  rsync --update -raz "$BASEDIR/" "${FOLDER}"
+  #rm -rf "$BASEDIR/photos"
+  #rm -rf "$BASEDIR/video"
   echo "Синхронизировано с ${BACKUPDIR}"
 else
   echo "Не синхронизировано."
